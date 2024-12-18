42db7a7abdf9f7e1f021c218e49bd45b249e2971
sahandmoslemi <sahandmoslemi@gmail.com>
2024-12-18 12:25:49 +0300

    feat: add the Dataset class to download the dataset

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from datetime import datetime
+from tqdm import tqdm
+
+# Root
+MASOUD = os.getcwd()
+
+# Data
+MASOUD_1 = os.path.join(MASOUD, "data")
+
+# TMP
+MASOUD_2 = os.path.join(MASOUD, "tmp")
+
+# Date Format
+MASOUD_5 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Logs
+MOSOUD_3 = os.path.join(MASOUD_2, "logs")
+MASOUD_4 = os.path.join(MOSOUD_3, f"{MASOUD_5}.log")
+
+logging.basicConfig(
+    filename=MASOUD_4, 
+    filemode='w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+tqdm.pandas(desc="Progress ...")
---
+++ preprocessing.py
@@ -0,0 +1,32 @@
+import kagglehub
+import os
+import logging
+import shutil
+from config import MASOUD_1
+
+
+class Masoud:
+    def __init__(self, masoud_2, masoud_1):
+        # Dataset Name
+        self.masoud_2 = masoud_2
+
+        # Data Path
+        self.masoud_1 = masoud_1
+
+        self._masoud()
+
+    def _masoud(self):
+
+        # Download if not exists
+        if not os.path.exists(self.masoud_1):
+            masoud_3 = kagglehub.dataset_download(self.masoud_2)
+
+            logging.info(f"Downloading {self.masoud_2} to {masoud_3}...")
+            logging.info(f"Copying {self.masoud_2} to {self.masoud_1}...")
+
+            shutil.copytree(masoud_3, self.masoud_1)
+        
+        logging.info(f"Data folder {self.masoud_2} already exists ...")
+
+if __name__ == "__main__":
+    Masoud("bharatkumar0925/tmdb-movies-clean-dataset", MASOUD_1)
