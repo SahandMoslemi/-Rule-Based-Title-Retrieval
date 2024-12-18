dae3fb5d57bce7f8aa53d8a42b2246c68bc9ce66
sahandmoslemi <sahandmoslemi@gmail.com>
2024-12-18 12:28:36 +0300

    fix

--- preprocessing.py
+++ preprocessing.py
@@ -25,8 +25,11 @@ class Masoud:
             logging.info(f"Copying {self.masoud_2} to {self.masoud_1}...")
 
             shutil.copytree(masoud_3, self.masoud_1)
-        
-        logging.info(f"Data folder {self.masoud_2} already exists ...")
+
+        else:
+            logging.info(f"Data folder {self.masoud_1} for {self.masoud_2} already exists ...")
+
+        return self.masoud_1
 
 if __name__ == "__main__":
     Masoud("bharatkumar0925/tmdb-movies-clean-dataset", MASOUD_1)
